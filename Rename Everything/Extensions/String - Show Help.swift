57a4cf641236222584a1bdb47d35c1676f6eb7c7
David Bureš <buresdv@gmail.com>
2024-04-24 17:13:12 +0200

    ~ Better help colors

--- Rename Everything/Extensions/String - Show Help.swift
+++ Rename Everything/Extensions/String - Show Help.swift
@@ -15,17 +15,19 @@ extension String
 
 \("Usage:".font([.bold]))
   renameEverything [optional arguments] [folder location]
+  \("By default, the app will rename all items in the target folder to random UUIDs.".color(.gray))
+  \("See below for other ways of renaming them".color(.gray))
 
 \("Optional Arguments:".font([.bold]))
   --renameType: Specify how files should be renamed
     \("Possible types:".font([.italic]).color(.blue))
-      randomUUID: Rename files to random UUIDs
-      randomWord: Rename files to random words
+      randomUUID: \("Rename files to random UUIDs".color(.gray))
+      randomWord: \("Rename files to random words".color(.gray))
     \("Example usage:".font([.italic]).color(.blue))
-      renameEverything --renameType randomUUID [folder location]
-        \("Will rename all files in the specified folder to random UUIDs".font([.italic]))
-      renameEverything --renameType randomWord [folder location]
-        \("Will rename all files in the specified folder to random English words".font([.italic]))
+      renameEverything \("--renameType randomUUID".color(.red)) [folder location]
+        \("Will rename all files in the specified folder to random UUIDs".font([.italic]).color(.gray))
+      renameEverything \("--renameType randomWord".color(.red)) [folder location]
+        \("Will rename all files in the specified folder to random English words".font([.italic]).color(.gray))
 
 \("Options:".font([.bold]))
   --help: Show this help
